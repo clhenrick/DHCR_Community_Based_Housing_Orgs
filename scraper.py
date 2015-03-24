3d1d408dcf10d61e06a3ede0a632281aa99f166c
Chris Henrick <chrishenrick@gmail.com>
2015-03-23 21:47:48 -0400

    add url & test css selector

--- scraper.py
+++ scraper.py
@@ -1,15 +1,17 @@
 # This is a template for a Python scraper on Morph (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("https://www1.dhcr.state.ny.us/LocalHousingOrgLists/CommBased.aspx?type=rent")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+table = root.cssselect("table[align='center']")
+
+print table
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
