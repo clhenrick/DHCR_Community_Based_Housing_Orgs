d4f6200335819d1edf6d3b18f62022f8a156a83d
Chris Henrick <chrishenrick@gmail.com>
2015-04-18 12:18:31 -0400

    update node script & json data

--- join_json.js
+++ join_json.js
@@ -1,8 +1,8 @@
 var joiner = require('joiner');
 var fs = require('fs');
 
-var list_data = JSON.parse(fs.readFileSync('hcr_comm_housing_org_list.json'));
-var deets_data = JSON.parse(fs.readFileSync('hcr_comm_housing_org_details.json'));
-var joined = joiner.left(list_data, 'name', deets_data, 'title');
+var list_data = JSON.parse(fs.readFileSync('json/hcr_comm_housing_org_list.json'));
+var deets_data = JSON.parse(fs.readFileSync('json/hcr_comm_housing_org_details.json'));
+var joined = joiner.left(list_data["HCR List of Community Based Housing Orgs"], 'name', deets_data["HCR Community Based Housing Org details"], 'title');
 
 console.log(JSON.stringify(joined));
